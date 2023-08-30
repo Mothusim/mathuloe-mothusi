b1b9e62283cf778d8f9c463b68b6870a99f7df27
Mothusim <mothusim97@gmail.com>
2023-08-31 01:04:59 +0200

    fixed the file structure to deploy in netlify

--- second/src/SendGrid.jsx
+++
@@ -1,3 +0,0 @@
-import sgMail from '@sendgrid/mail';
-
-sgMail.setApiKey('YOUR_SENDGRID_API_KEY');
--- second/src/supabase.jsx
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-// Use a custom domain as the supabase URL
-const supabase = createClient('https://mkgcgxyvnomhfhkpiyjy.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZ2NneHl2bm9taGZoa3BpeWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIyODA5OTcsImV4cCI6MjAwNzg1Njk5N30._a50IRBJGQe38aPRdaF2BPOWDuWrBaV_VBkHhFU6x4U')
-
-export default supabase
