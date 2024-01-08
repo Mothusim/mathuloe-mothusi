1a9cf793ac78c587b1dce54406cc7f43d4c5969c
Mothusi Mathuloe <111005114+Mothusim@users.noreply.github.com>
2024-01-08 15:10:40 +0200

    Added a chatBot

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,6 +1,7 @@
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import SideNav from '../components/SideNav'
+import { BubbleChat } from 'flowise-embed-react'
 
 export default function AppLayout(){
 
@@ -17,6 +18,8 @@ export default function AppLayout(){
                 <Outlet />
                 
             </main>
+            <BubbleChat chatflowid="858d9e66-48ee-4c27-8da0-42a05d358802" apiHost="https://flowise-f0nl.onrender.com" />
+
 
         </>
 
