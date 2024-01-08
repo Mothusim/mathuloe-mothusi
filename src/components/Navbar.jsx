656e4919dfa49eec4919d54e3ed3abdbe01c3a39
Mothusi Mathuloe <111005114+Mothusim@users.noreply.github.com>
2024-01-08 15:15:44 +0200

    Fixed layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import GithubIcon from '../assets/github.svg'
 import linkedinIcon from '../assets/linkedin.svg'
 import TwitterIcon from '../assets/twitter.svg'
+import { BubbleChat } from 'flowise-embed-react'
 
 const socialIcons = {
 
@@ -39,6 +40,7 @@ export default function NavBar(){
                     <a href='https://github.com/Mothusim' target="_blank" rel="noreferrer"><img src={GithubIcon} style={socialIcons} alt="Github icon" /></a>
                     <a href='http://www.linkedin.com/in/mothusi-mathuloe' target="_blank" rel="noreferrer"><img src={linkedinIcon} style={socialIcons} alt="Linkedin icon" /></a>
                     <a href='#' target="_blank" rel="noreferrer"><img src={TwitterIcon} style={socialIcons} alt="Twitter icon" /></a>
+                    <BubbleChat chatflowid="858d9e66-48ee-4c27-8da0-42a05d358802" apiHost="https://flowise-f0nl.onrender.com" />
 
                 </Nav>
 
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,7 +1,6 @@
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import SideNav from '../components/SideNav'
-import { BubbleChat } from 'flowise-embed-react'
 
 export default function AppLayout(){
 
@@ -18,7 +17,7 @@ export default function AppLayout(){
                 <Outlet />
                 
             </main>
-            <BubbleChat chatflowid="858d9e66-48ee-4c27-8da0-42a05d358802" apiHost="https://flowise-f0nl.onrender.com" />
+            
 
 
         </>
